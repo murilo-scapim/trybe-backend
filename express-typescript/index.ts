5ceab0d18488ad57510bb097ab0e9d130f346367
Murilo <mhscapim@gmail.com>
2022-07-24 15:49:22 -0300

    Adicionado middleware de erro

--- express-typescript/index.ts
+++ express-typescript/index.ts
@@ -1,5 +1,6 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import 'express-async-errors';
 
 const app = express();
 
@@ -9,6 +10,26 @@ const PORT = 8000;
 
 app.get('/', (req: Request, res: Response) => {
     res.status(StatusCodes.OK).send('Express + TypeScript');
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    const { name, message, details } = err as any;
+
+    switch (name) {
+        case 'ValidationError':
+            res.status(400).json({ message: details[0].message });
+            break;
+        case 'NotFoundError':
+            res.status(404).json({ message });
+            break;
+        case 'ConflictError':
+            res.status(409).json({ message });
+            break;
+        default:
+            console.log(err);
+            res.sendStatus(500);
+    }
+    next();
 });
 
 app.listen(PORT, () => {
