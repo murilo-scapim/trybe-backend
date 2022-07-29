081204b0e60933b17c2df51fd52ce965228bee64
Murilo <murilo.scapim@outlook.com>
2022-07-29 18:08:56 -0300

    Criado arquivo app.ts e configurada a conexão com o banco de dados

---
+++ express-typescript-restaurants/src/app.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(StatusCodes.OK).send('Its alive!');
+});
+
+export default app;
