306f50efc45c2ebefb64c35ff3f1dee0b0e730c2
Murilo <murilo.scapim@outlook.com>
2022-07-29 19:49:35 -0300

    Refatoração

--- express-typescript-restaurants/src/controllers/restaurant.controller.ts
+++ express-typescript-restaurants/src/controllers/restaurant.controller.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import { Request, Response } from "express";
 import RestaurantService from "../services/restaurant.service";
 
 class RestaurantController {
